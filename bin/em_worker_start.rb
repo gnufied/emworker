c3022141076d68f0154babae5e4d31949942263a
gnufied <gethemant@gmail.com>
2008-09-28 00:33:02 +0530

    pass command line arguments

--- bin/em_worker_start.rb
+++ bin/em_worker_start.rb
@@ -4,4 +4,4 @@ dir = File.dirname(__FILE__)
 $:.unshift(File.join(dir,"..","lib"))
 require "em_worker"
 WORKER_ROOT = File.join(dir,"..","workers")
-EmWorker::EmMaster.start_master_process
+EmWorker::EmMaster.start_master_process(ARGV)
